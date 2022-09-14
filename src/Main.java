aad0cf8b5c38b5e7d26b7a71f390a948ebfe2c59
Anastasiya Saakyan <vv999vv@bk.ru>
2022-09-14 22:15:22 +0300

    Added demonstration in Main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String TEXT = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                "Ut enim ad minim veniam, " +
+                "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit " +
+                "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, " +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        WordsChecker wordsChecker = new WordsChecker(TEXT);
+
+        System.out.println("Есть ли в тексте слово Lorem:" + (wordsChecker.hasWord("Lorem") ?   " да" : " нет"));
+        System.out.println("Есть ли в тексте слово elit:" + (wordsChecker.hasWord("elit") ?   " да" : " нет"));
+        System.out.println("Есть ли в тексте слово Один:" + (wordsChecker.hasWord("Один") ?  " да" : " нет"));
+        System.out.println("Есть ли в тексте слово red:" + (wordsChecker.hasWord("red") ?  " да" : " нет"));
+    }
+}
